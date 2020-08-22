10881724e51380960b056577c6f7478cdb516849
ahmad alfan <alfanjuni@gmail.com>
2020-08-23 00:08:47 +0700

    coba index db

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import { default as localforage } from 'localforage';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [value, setValue] = useState('');
+	const [retriveValue, setRetriveValue] = useState('');
+	const p = null;
+
+	console.log('log', value);
+
+	const handleChange = (e) => {
+		setValue(e.target.value);
+	};
+
+	const handleOnSubmit = (e) => {
+		e.preventDefault();
+		localforage
+			.setItem('somekey', value)
+			.then(function (value) {
+				console.log('submit', value);
+			})
+			.catch(function (err) {
+				console.log(err);
+			});
+	};
+
+	const handleClick = () => {
+		localforage
+			.getItem('somekey')
+			.then((val) => {
+				console.log(val);
+				setRetriveValue(val);
+			})
+			.catch(function (err) {
+				console.log(err);
+			});
+	};
+	return (
+		<div>
+			<form onSubmit={handleOnSubmit}>
+				<label>Enter Favorite Color</label>
+				<input onChange={handleChange} type="text" />
+				<input type="submit" />
+			</form>
+			<button onClick={handleClick}>Retrieve</button>
+			<p>{retriveValue}</p>
+		</div>
+	);
 }
 
 export default App;
